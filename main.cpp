4a44289f2dd1b30c95e435cc51de65cb20d87272
eg0rmeister <karpuhin.egor@gmail.com>
2022-10-21 20:29:21 +0300

    Added an InputSequence function for all the inputs

--- main.cpp
+++ main.cpp
@@ -17,14 +17,18 @@ int* BinarySearch(int* begin, int* end) {
   return begin;
 }
 
-int main() {
+void input_sequence(int* array) {
   size_t array_length;
   cin >> array_length;
-  int* int_array = new int[array_length + 1];
   for (size_t i = 0; i < array_length; i++) {
     cin >> int_array[i];
   }
   cout << "\n";
+}
+
+int main() {
+  int* int_array = new int[array_length + 1];
+  input_sequence(int_array);
   int* result = BinarySearch(int_array, int_array + (array_length - 1));
   cout << result - int_array << endl;
   delete[](int_array);
